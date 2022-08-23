9d21b6f01ae52c03d6adcc2a9e4d95ab2221d166
Md Mafizur Rahman <105044128+rahman-unilu@users.noreply.github.com>
2022-08-23 10:09:52 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'UL Rucio Tutorial'
+copyright = '2022, Uni.lu'
+author = 'Uni.lu'
 
 release = '0.1'
 version = '0.1.0'
